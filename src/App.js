0be43958b4e0e766b7800aaae72bc6d3d1d1f456
Mateosalinasm <mateo.salinas95@gmail.com>
2023-04-08 17:02:56 -0500

    Added a mission statement to the home page

--- src/App.js
+++ src/App.js
@@ -12,16 +12,22 @@ function App() {
       <nav className="fade"></nav>
       <Parallax pages={2}>
         <ParallaxLayer speed={0.2}>
-          <WorkoutBanner />
-        </ParallaxLayer>
-        <ParallaxLayer className="test" offset={0.9999} speed={1}>
           <MotionAnimate
-            delay={1.7}
+            delay={1}
             speed={2}
             ease={[0.75, 0.45, 0.53, 0.94]}
             reset={true}
           >
             <Video />
+          </MotionAnimate>
+        </ParallaxLayer>
+        <ParallaxLayer className="test" offset={0.8} speed={0.7}>
+          <MotionAnimate
+            delay={2.5}
+            speed={2}
+            ease={[0.75, 0.45, 0.53, 0.94]}
+            reset={true}>
+          <WorkoutBanner />
           </MotionAnimate>
         </ParallaxLayer>
       </Parallax>
