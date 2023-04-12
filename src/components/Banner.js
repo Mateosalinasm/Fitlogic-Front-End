d748d01cee2ec37847518d6f5b8eb1c1b1897120
Mateosalinasm <mateo.salinas95@gmail.com>
2023-04-11 23:11:41 -0500

    added a motivational banner

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,7 +1,15 @@
+import Image from "../components/Image";
+
 const Banner = () => {
     return(
-        <div className="banner-container">
-            <div className="banner"></div>
+        <div className="motivational-banner-container">
+            <div className="motivational-banner">
+                <h1 className="title">Live a healthier life with FITLOGIC</h1>
+                <h4 className="motivational-quote">"Motivation is what gets you started. Habit is what keeps you going." - Jim Ryun</h4>
+            </div>
+                <div className="banner-image">
+                    <Image/>
+                </div>
         </div>
     )
 }
---
+++ src/components/Image.js
@@ -0,0 +1,5 @@
+const Image = () => {
+    return <img className="banner-image" src="/assets/images/10.png" alt="" />;
+}
+
+export default Image
