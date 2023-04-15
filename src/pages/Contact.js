a1aef881cb78bf9ae4c3154b3f445306b774b0dd
10xXAhmedXx10 <ahmed.salihi9610@gmail.com>
2023-04-15 14:31:45 -0600

    submit button-not-finshed

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,7 +1,58 @@
+import { useState } from 'react';
+
 const Contact = () => {
-    return (
-<h1>Welcome to contact</h1>
-    )
-}
+  const [submitted, setSubmitted] = useState(false);
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    setSubmitted(true);
+  };
+     
+  if (submitted) {
+    return <p className="wrapper-message">Thank you for your message,we will get back to you ASAP</p>;
+  }
+
+  return (
+    <div className='wrapper-form'>
+    <form className="contact-form">
+      <div className="form-group">
+        <label htmlFor="name">Name</label>
+        <input type="text" id="name" name="name" required />
+      </div>
+      <div className="form-group">
+        <label htmlFor="email">Email</label>
+        <input type="email" id="email" name="email" required />
+      </div>
+      <div className="form-group">
+        <label htmlFor="description">Message</label>
+        <textarea id="description" name="description" required />
+      </div>
+      
+    </form>
+
+    <div class="container-1">
+  <a href="#" class="button" onClick={handleSubmit}>
+    <div class="plate"></div>
+    <div class="plate"></div>
+    <div class="plate"></div>
+    <div class="plate"></div>
+    <div class="plate"></div>
+    <div class="button__wrapper">
+      <span class="button__text">submit</span>
+    </div>
+    <div class="button__box">
+      <div class="inner inner__top"></div>
+      <div class="inner inner__front"></div>
+      <div class="inner inner__bottom"></div>
+      <div class="inner inner__back"></div>
+      <div class="inner inner__left"></div>
+      <div class="inner inner__right"></div>
+    </div>
+  </a>
+</div>
+
+    </div>
+  );
+};
 
 export default Contact;
