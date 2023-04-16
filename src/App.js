c1d8746769c09cb97abbfcb4b7129fa060823080
Mateosalinasm <mateo.salinas95@gmail.com>
2023-04-16 03:09:25 -0500

    Created sign in page and added hover effect to the form container

--- src/App.js
+++ src/App.js
@@ -1,29 +1,29 @@
 import { useState } from "react";
-import { Route, Routes } from 'react-router-dom'
-import Navbar from './components/Navbar.js'
-import Home from './pages/Home';
-import Workout from './pages/Workout';
-import AddWorkout from './pages/AddWorkout';
+import { Route, Routes, useLocation } from "react-router-dom";
+import Navbar from "./components/Navbar.js";
+import Home from "./pages/Home";
+import Workout from "./pages/Workout";
+import AddWorkout from "./pages/AddWorkout";
 import HealthyTips from "./pages/HealthyTips.js";
-import About from './pages/About';
-import Contact from './pages/Contact';
-import SignIn from './pages/SignIn';
-// import Register from './components/Register';
+import About from "./pages/About";
+import Contact from "./pages/Contact";
+import SignIn from "./pages/SignIn";
+import Register from "./pages/Register.js";
 
 import "./App.css";
 
 function App() {
+  const location = useLocation();
 
-  // const [activePage, setActivePage] = useState("home");
-
-  // const handleLinkClick = (page) => {
-  //   setActivePage(page);
-  // };
+  // Check if current location is the sign-in page
+  const isSignInPage = location.pathname === "/sign-in";
+  const isRegisterPage = location.pathname === "/register";
 
   return (
     <div className="App">
-      <Navbar  />
-      <nav className="fade"></nav>
+      {/* Render Navbar only if not on the sign-in or register page */}
+      {!isSignInPage && !isRegisterPage && <Navbar />}
+      <nav className={isSignInPage || isRegisterPage ? null : "fade"}></nav>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/workout" element={<Workout />} />
@@ -31,9 +31,9 @@ function App() {
         <Route path="/healthy-tips" element={<HealthyTips />} />
         <Route path="/about" element={<About />} />
         <Route path="/contact" element={<Contact />} />
-        <Route path="/signIn" element={<SignIn />} />
+        <Route path="/sign-in" element={<SignIn />} />
+        <Route path="/register" element={<Register />} />
       </Routes>
-      {/* <Register /> */}
     </div>
   );
 }
--- src/pages/SignIn.js
+++ src/pages/SignIn.js
@@ -1,6 +1,38 @@
+import VanillaTilt from "../vanilla-tilt";
+import { useEffect } from "react";
+import {motion} from 'framer-motion'
 const SignIn = () => {
+    useEffect(() => {
+    // Initialize VanillaTilt on the tilt element
+    VanillaTilt.init(document.querySelector('[data-tilt]'));
+    }, []);
     return (
-<h1>Welcome to signin</h1>
+        <div className="sign-in-container">
+            <img className="sign-in-background" src="/assets/images/icons/Untitled design (3).png" alt="" />
+            <div data-tilt data-tilt-scale="1.01" data-tilt-max="10" data-tilt-glare data-tilt-max-glare="0.3"  className="sign-in">
+                <div>
+                    <div>
+                        <h1 className="sign-up-title">Sign In</h1>
+                        <div className="registration login-btns sign-up-layout">
+                            <form id="log-in-form" >
+                                <fieldset className="account-info">
+                                    <input placeholder="USERNAME" className="sign-up-inputs" name="username" type="text" />
+                                    <input placeholder="PASSWORD" className="sign-up-inputs" name="password" type="password" />
+                                </fieldset>
+                            </form>
+                        </div>
+                        <motion.button
+                            whileHover={{ scale: 1.05 }}
+                            whileTap={{ scale: 0.8 }}
+                            className="menu-btn"
+                        >
+                        <input className="req-btn log-in-btn" type="submit" value="Log in" form="log-in-form"/>
+                        </motion.button>
+                    </div>
+                </div>
+            </div>
+            <script type="text/javascript" src="vanilla-tilt.js"></script>
+        </div>
     )
 }
 
