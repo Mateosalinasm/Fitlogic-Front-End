b2b25ea12426b5ba86c379539b3422d379357714
10xXAhmedXx10 <ahmed.salihi9610@gmail.com>
2023-04-18 01:53:52 -0600

    changing the submit icon + the contact menu

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -7,50 +7,35 @@ const Contact = () => {
     event.preventDefault();
     setSubmitted(true);
   };
-     
+
   if (submitted) {
-    return <img className="wrapper-message" src='/assets/FitLogic-contactusmessage.gif'></img>;
+    return (
+        <img className='wrapper-message' src="/assets/FitLogic-contactusmessage.gif" alt="Thank you for your message" />
+    );
   }
 
   return (
     <div className='wrapper-form'>
-    <form className="contact-form">
-      <div className="form-group">
-        <label htmlFor="name">Name</label>
-        <input type="text" id="name" name="name" required />
+      <div className='form-container'>
+        <form className="contact-form" onSubmit={handleSubmit}>
+          <div className="form-group">
+            <label htmlFor="name">Name</label>
+            <input type="text" id="name" name="name" required />
+          </div>
+          <div className="form-group">
+            <label htmlFor="email">Email</label>
+            <input type="email" id="email" name="email" required />
+          </div>
+          <div className="form-group">
+            <label htmlFor="description">Message</label>
+            <textarea id="description" name="description" required />
+          </div>
+          <div class="wrap">
+          <button class="submit">Submit</button>
+         </div>
+        </form>
+        <img className='form-image' src="/assets/contact-us.jpg" alt="Contact us" />
       </div>
-      <div className="form-group">
-        <label htmlFor="email">Email</label>
-        <input type="email" id="email" name="email" required />
-      </div>
-      <div className="form-group">
-        <label htmlFor="description">Message</label>
-        <textarea id="description" name="description" required />
-      </div>
-      
-    </form>
-
-    <div class="container-1">
-  <a href="#" class="button" onClick={handleSubmit}>
-    <div class="plate"></div>
-    <div class="plate"></div>
-    <div class="plate"></div>
-    <div class="plate"></div>
-    <div class="plate"></div>
-    <div class="button__wrapper">
-      <span class="button__text">Submit</span>
-    </div>
-    <div class="button__box">
-      <div class="inner inner__top"></div>
-      <div class="inner inner__front"></div>
-      <div class="inner inner__bottom"></div>
-      <div class="inner inner__back"></div>
-      <div class="inner inner__left"></div>
-      <div class="inner inner__right"></div>
-    </div>
-  </a>
-</div>
-
     </div>
   );
 };
