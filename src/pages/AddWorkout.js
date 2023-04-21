a775384eba2662ba318015e43d8a22923d2618d5
Ahmed <ahmed.salihi9610@gmail.com>
2023-04-21 16:33:31 -0600

    full crud is  complete and working

--- src/pages/AddWorkout.js
+++ src/pages/AddWorkout.js
@@ -3,6 +3,7 @@ import axios from 'axios';
 
 const AddWorkout = () => {
   const [workouts, setWorkouts] = useState([]);
+  const [editing, setEditing] = useState(null);
   const [formData, setFormData] = useState({
     name: '',
     workout: '',
@@ -40,12 +41,24 @@ const AddWorkout = () => {
       alert('Error adding workout');
     }
   };
-  
 
-  const handleUpdate = async (id, updatedData) => {
+  const handleEdit = (workout) => {
+    setEditing(workout._id);
+    setFormData({
+      name: workout.name,
+      workout: workout.workout,
+      reps: workout.reps,
+      youtubeVideo: workout.youtubeVideo,
+    });
+  };
+
+  const handleUpdate = async (e) => {
+    e.preventDefault();
     try {
-      await axios.put(`http://localhost:5000/workouts/${id}`, updatedData);
+      await axios.put(`http://localhost:5000/workouts/${editing}`, formData);
       alert('Workout updated successfully!');
+      setEditing(null);
+      setFormData({ name: '', workout: '', reps: '', youtubeVideo: '' });
       fetchWorkouts();
     } catch (error) {
       console.error(error);
@@ -64,45 +77,97 @@ const AddWorkout = () => {
     }
   };
 
+  const extractVideoId = (url) => {
+    const regex = /(https?:\/\/)?(www\.)?(youtube\.com|youtu\.?be)\/.+$/;
+    const match = url.match(regex);
+    if (match) {
+      const parsedUrl = new URL(url);
+      return parsedUrl.searchParams.get("v");
+    }
+    return null;
+  };
+
   return (
     <div className="add-workout">
-      <h2>Add Workout</h2>
-      <form onSubmit={handleSubmit} className="workout-form">
-        <input type="text" name="name" placeholder="Name" value={formData.name} onChange={handleChange} required className="input-field" />
-        <input type="text" name="workout" placeholder="Workout" value={formData.workout} onChange={handleChange} required className="input-field" />
-        <input type="number" name="reps" placeholder="Reps" value={formData.reps} onChange={handleChange} required className="input-field" />
-        <input type="text" name="youtubeVideo" placeholder="YouTube Video URL" value={formData.youtubeVideo} onChange={handleChange} required className="input-field" />
-        <button type="submit" className="submit-btn">Add Workout</button>
-      </form>
-
-      <h2>All Workouts</h2>
-      <table>
-        <thead>
-          <tr>
-            <th>Name</th>
-            <th>Workout</th>
-            <th>Reps</th>
-            <th>YouTube Video URL</th>
-            <th>Actions</th>
-          </tr>
-        </thead>
-        <tbody>
-          {workouts.map((workout) => (
-            <tr key={workout._id}>
-              <td>{workout.name}</td>
-              <td>{workout.workout}</td>
-              <td>{workout.reps}</td>
-              <td>{workout.youtubeVideo}</td>
-          <td>
-            <button onClick={() => handleUpdate(workout._id, { name: 'New Name' })}>Update</button>
-            <button onClick={() => handleDelete(workout._id)}>Delete</button>
-          </td>
-        </tr>
-      ))}
-    </tbody>
-  </table>
-</div>
+      {editing ? (
+        <h2>Edit Workout</h2>
+      ) : (
+        <h2>Add Workout</h2>
+      )}
+      <form onSubmit={editing ? handleUpdate : handleSubmit} className="workout-form">
+        <input
+          type="text"
+          name="name"
+          placeholder="Name"
+          value={formData.name}
+          onChange={handleChange}
+          required
+          className="input-field"
+        />
+        <input
+          type="text"
+          name="workout"
+          placeholder="Workout"
+          value={formData.workout}
+          onChange={handleChange}
+          required
+          className="input-field"
+        />
+        <input
+          type="number"
+          name="reps"
+          placeholder="Reps"
+          value={formData.reps}
+          onChange={handleChange}
+          required
+          className="input-field"
+          />
+          <input
+            type="text"
+            name="youtubeVideo"
+            placeholder="YouTube Video URL"
+            value={formData.youtubeVideo}
+            onChange={handleChange}
+            required
+            className="input-field"
+          />
+          <button type="submit" className="submit-btn">
+            {editing ? 'Update Workout' : 'Add Workout'}
+          </button>
+        </form>
+        <div className="workout-list">
+          <h2>Workouts</h2>
+          <ul className='ul-From-List'>
+            {workouts.map((workout) => (
+              <li className='li-list' key={workout._id}>
+                <div className="workout-info">
+                  <h3>{workout.name}</h3>
+                  <p>{workout.workout}</p>
+                  <p>{workout.reps}</p>
+                  <p>{workout.youtubeVideo}</p>
+                  {workout.youtubeVideo && (
+                    <div className="video-wrapper">
+                      <iframe
+                        width="560"
+                        height="315"
+                        src={`https://www.youtube.com/embed/${extractVideoId(workout.youtubeVideo)}`}
+                        title={workout.name}
+                        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                        allowFullScreen
+                      ></iframe>
+                    </div>
+                  )}
+                </div>
+                <div className="workout-actions">
+                  <button onClick={() => handleEdit(workout)}>Edit</button>
+                  <button onClick={() => handleDelete(workout._id)}>Delete</button>
+                </div>
+              </li>
+            ))}
+          </ul>
+        </div>
+      </div>
 );
 };
 
-export default AddWorkout;
+export default AddWorkout;      
