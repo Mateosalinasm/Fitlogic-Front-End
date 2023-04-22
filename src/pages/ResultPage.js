0700133e40525895a7ac77f65d283e9c65831650
ellesiba <rochelle.sibaja@gamil.com>
2023-04-21 20:53:34 -0400

    To update

--- src/pages/ResultPage.js
+++ src/pages/ResultPage.js
@@ -1,49 +1,13 @@
-// import React from 'react';
-// import { useLocation } from 'react-router-dom';
-// import ExerciseList from '../components/ExerciseList';
-
-// const ResultPage = () => {
-//   const location = useLocation();
-//   const searchResults = location.state;
-
-//   return (
-//     <div>
-//       <h1>Search Results</h1>
-//       {searchResults ? (
-//         <ExerciseList exercises={searchResults} />
-//       ) : (
-//         <p>No results found.</p>
-//       )}
-//     </div>
-//   );
-// };
-
-// export default ResultPage;
-
 
 import React from 'react';
-import { useLocation } from 'react-router-dom';
+// import { useLocation } from 'react-router-dom';
 import ExerciseList from '../components/ExerciseList';
+// import "./App.css";
 
 const ResultPage = (props) => {
-//   const location = useLocation();
-//   // const searchResults = location.state.data || [];
-//   function SearchResult(props) {
-//     const { results } = props;
-//     return (
-//       <div>
-//         {results.map((result) => (
-//           <div key={result.id}>
-//             <p>{result.Name?.First}</p>
-//             <p>{result.Name?.Last}</p>
-//           </div>
-//         ))}
-//       </div>
-//     );
-//   }  
 
   return (
-    <div>
+    <div className='search-render'>
       {props.dataState ?(
         <ExerciseList exercise={props.dataState} />
       ) : (
